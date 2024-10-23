57dbb7a3e44b90b4efa48aadd39a81571c350325
Swetha Ganapathi Raman <swethag04@gmail.com>
2024-10-23 12:33:52 -0700

    remove other routes

--- 2_templates/smartcookielearner/smartcookie/routes.py
+++ 2_templates/smartcookielearner/smartcookie/routes.py
@@ -5,19 +5,3 @@ from smartcookie import app
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
-
-@app.route('/quiz')
-def quiz():
-    return render_template('quiz.html', title='Quiz')
-
-@app.route('/coding')
-def coding():
-    return render_template('coding.html', title='Coding')
-
-@app.route('/ai')
-def AI():
-    return render_template('ai.html', title='ai')
-
-@app.route('/profile')
-def profile():
-    return render_template('profile.html', title='Profile')
